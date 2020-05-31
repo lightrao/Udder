e857152a0e05d217f30f19bc2c971f49667e074f
LightRao <light@hotmail.com>
2020-05-31 22:38:51 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 饶阳 on 2020/5/31.
+//  Copyright © 2020 LightRao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
